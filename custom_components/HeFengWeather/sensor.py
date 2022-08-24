e80d5d03f3114aeeaea70c445df432695c67f6f3
Vaciller <wyc_19533480830@outlook.com>
2022-08-24 21:39:20 +0800

    增加空气质量模组

--- custom_components/HeFengWeather/sensor.py
+++ custom_components/HeFengWeather/sensor.py
@@ -32,31 +32,31 @@ CONF_DISASTERMSG   = "disastermsg"
 
 # 定义三个可选项：温度、湿度、PM2.5
 OPTIONS = {
-    "temprature": ["HeFengweather_temperature", "室外温度"    , "mdi:thermometer"           , TEMP_CELSIUS],
-    "humidity"  : ["HeFengweather_humidity"   , "室外湿度"    , "mdi:water-percent"         , "%"         ],
-    "feelsLike" : ["HeFengweather_feelsLike"  , "体感温度"    , "mdi:thermometer"           , TEMP_CELSIUS],
-    "icon"      : ["HeFengweather_icon"       , "天气图标"    , None                        , None        ],
-    "text"      : ["HeFengweather_text"       , "天气描述"    , None                        , None        ],
-    "wind360"   : ["HeFengweather_wind360"    , "风向360角度" , "mdi:weather-windy"         , None        ],
-    "windDir"   : ["HeFengweather_windDir"    , "风向"        , "mdi:weather-windy"         , None        ],
-    "windScale" : ["HeFengweather_windScale"  , "风力等级"    , "mdi:weather-windy"         , None        ],
-    "windSpeed" : ["HeFengweather_windSpeed"  , "风速"        , "mdi:weather-windy"         , "km/h"      ],
-    "precip"    : ["HeFengweather_precip"     , "此小时降水量" , "mdi:weather-pouring"      , "mm"        ],
-    "pressure"  : ["HeFengweather_pressure"   , "大气压强"    , "mdi:car-brake-low-pressure", "hPa"       ],
-    "vis"       : ["HeFengweather_vis"        , "能见度"      , "mdi:eye-outline"           , "km"        ],
-    "cloud"     : ["HeFengweather_cloud"      , "云量"        , "mdi:clouds"                , None        ],
-    "dew"       : ["HeFengweather_dew"        , "露点温度"    , "mdi:thermometer"           , None        ],
+    "temprature": ["HeFengweather_temperature", "室外温度", "mdi:thermometer", TEMP_CELSIUS],
+    "humidity": ["HeFengweather_humidity", "室外湿度", "mdi:water-percent", "%"],
+    "feelsLike": ["HeFengweather_feelsLike", "体感温度", "mdi:thermometer", TEMP_CELSIUS],
+    # "icon": ["HeFengweather_icon", "天气图标", None, None],
+    # "text": ["HeFengweather_text", "天气描述", None, None],
+    "wind360": ["HeFengweather_wind360", "风向360角度", "mdi:weather-windy", None],
+    "windDir": ["HeFengweather_windDir", "风向", "mdi:weather-windy", None],
+    "windScale": ["HeFengweather_windScale", "风力等级", "mdi:weather-windy", None],
+    "windSpeed": ["HeFengweather_windSpeed", "风速", "mdi:weather-windy", "km/h"],
+    "precip": ["HeFengweather_precip", "此小时降水量", "mdi:weather-pouring", "mm"],
+    "pressure": ["HeFengweather_pressure", "大气压强", "mdi:car-brake-low-pressure", "hPa"],
+    "vis": ["HeFengweather_vis", "能见度", "mdi:eye-outline", "km"],
+    "cloud": ["HeFengweather_cloud", "云量", "mdi:clouds", None],
+    "dew": ["HeFengweather_dew", "露点温度", "mdi:thermometer", None],
+    "qlty": ["Heweather_qlty", "空气质量指数", "mdi:quality-high", None],
+    "level": ["Heweather_level", "空气质量指数等级", "mdi:walk", None],
+    "category": ["Heweather_category", "空气质量指数级别", "mdi:walk", None],
+    "primary": ["Heweather_primary", "空气质量的主要污染物", "mdi:walk", None],
+    "pm10": ["Heweather_pm10", "PM10", "mdi:emoticon-dead", "μg/m3"],
+    "pm25": ["Heweather_pm25", "PM2.5", "mdi:emoticon-dead", "μg/m3"],
+    "no2": ["Heweather_no2", "二氧化氮", "mdi:emoticon-dead", "μg/m3"],
+    "so2": ["Heweather_so2", "二氧化硫", "mdi:emoticon-dead", "μg/m3"],
+    "co": ["Heweather_co", "一氧化碳", "mdi:emoticon-dead", "μg/m3"],
+    "o3": ["Heweather_o3", "臭氧", "mdi:weather-cloudy", "μg/m3"],
     
-    # "primary": ["Heweather_primary", "空空气质量的主要污染物", "mdi:walk", " "],
-    # "category": ["Heweather_category", "空气质量指数级别", "mdi:walk", " "],
-    # "level": ["Heweather_level", "空气质量指数等级", "mdi:walk", " "],
-    # "pm25": ["Heweather_pm25", "PM2.5", "mdi:walk", "μg/m3"],
-    # "pm10": ["Heweather_pm10", "PM10", "mdi:walk", "μg/m3"],
-    # "no2": ["Heweather_no2", "二氧化氮", "mdi:emoticon-dead", "μg/m3"],
-    # "so2": ["Heweather_so2", "二氧化硫", "mdi:emoticon-dead", "μg/m3"],
-    # "co": ["Heweather_co", "一氧化碳", "mdi:emoticon-dead", "μg/m3"],
-    # "o3": ["Heweather_o3", "臭氧", "mdi:weather-cloudy", "μg/m3"],
-    # "qlty": ["Heweather_qlty", "综合空气质量", "mdi:quality-high", " "],
     "disaster_warn": ["Heweather_disaster_warn", "灾害预警", "mdi:warning-outline", " "],
     }
 
@@ -164,10 +164,10 @@ class HeweatherWeatherSensor(Entity):
             self._state = self._data.humidity
         elif self._type == "feelsLike":
             self._state = self._data.feelsLike
-        elif self._type == "icon":
-            self._state = self._data.icon
-        elif self._type == "text":
-            self._state = self._data.text
+        # elif self._type == "icon":
+        #     self._state = self._data.icon
+        # elif self._type == "text":
+        #     self._state = self._data.text
         elif self._type == "wind360":
             self._state = self._data.wind360
         elif self._type == "windDir":
@@ -186,28 +186,29 @@ class HeweatherWeatherSensor(Entity):
             self._state = self._data.cloud
         elif self._type == "dew":
             self._state = self._data.dew
-        # elif self._type == "category":
-        #     self._state = self._data.category
-        # elif self._type == "primary":
-        #     self._state = self._data.primary
-        # elif self._type == "level":
-        #     self._state = self._data.level
-        # elif self._type == "pm10":
-        #     self._state = self._data.pm10
-        # elif self._type == "pm25":
-        #     self._state = self._data.pm25
-        # elif self._type == "no2":
-        #     self._state = self._data.no2
-        # elif self._type == "so2":
-        #     self._state = self._data.so2
-        # elif self._type == "co":
-        #     self._state = self._data.co
-        # elif self._type == "o3":
-        #     self._state = self._data.o3
-        # elif self._type == "qlty":
-        #     self._state = self._data.qlty
-        # elif self._type == "disaster_warn":
-        #     self._state = self._data.disaster_warn
+        elif self._type == "qlty":
+            self._state = self._data.qlty
+        elif self._type == "level":
+            self._state = self._data.level
+        elif self._type == "category":
+            self._state = self._data.category
+        elif self._type == "primary":
+            self._state = self._data.primary
+        elif self._type == "pm10":
+            self._state = self._data.pm10
+        elif self._type == "pm25":
+            self._state = self._data.pm25
+        elif self._type == "no2":
+            self._state = self._data.no2
+        elif self._type == "so2":
+            self._state = self._data.so2
+        elif self._type == "co":
+            self._state = self._data.co
+        elif self._type == "o3":
+            self._state = self._data.o3
+        
+        elif self._type == "disaster_warn":
+            self._state = self._data.disaster_warn
 
 # 天气相关的数据
 class WeatherData(object):
@@ -224,8 +225,8 @@ class WeatherData(object):
         self._temprature = None
         self._humidity   = None
         self._feelsLike  = None
-        self._icon       = None
-        self._text       = None
+        # self._icon       = None
+        # self._text       = None
         self._wind360    = None
         self._windDir    = None
         self._windScale  = None
@@ -235,16 +236,17 @@ class WeatherData(object):
         self._vis        = None
         self._cloud      = None
         self._dew        = None
-        # self._category = None 
-        # self._pm10 = None
-        # self._primary = None
-        # self._level = None
-        # self._pm25 = None
-        # self._no2 = None
-        # self._so2 = None
-        # self._co = None
-        # self._o3 = None
-        # self._qlty = None
+        self._qlty       = None
+        self._level      = None
+        self._category   = None
+        self._primary    = None
+        self._pm10       = None
+        self._pm25       = None
+        self._no2        = None
+        self._so2        = None
+        self._co         = None
+        self._o3         = None
+        
         self._disaster_warn = None
         
 
@@ -318,55 +320,55 @@ class WeatherData(object):
     def dew(self):
         return self._dew
 
-    # @property
-    # def category(self):
-    #     """空气质量指数级别"""
-    #     return self._category
-    
-    # @property
-    # def level(self):
-    #     """空气质量指数等级"""
-    #     return self._level
+    # 空气质量指数
+    @property
+    def qlty(self):
+        return self._qlty
 
-    # @property
-    # def primary(self):
-    #     """空气质量的主要污染物，空气质量为优时，返回值为NA"""
-    #     return self._primary
+    # 空气质量指数等级
+    @property
+    def level(self):
+        return self._level
 
-    # @property
-    # def pm25(self):
-    #     """pm2.5"""
-    #     return self._pm25
+    # 空气质量的主要污染物
+    @property
+    def primary(self):
+        return self._primary
 
-    # @property
-    # def pm10(self):
-    #     """pm10"""
-    #     return self._pm10
-    
-    # @property
-    # def qlty(self):
-    #     """(aqi)空气质量指数"""
-    #     return self._qlty
-    
-    # @property
-    # def no2(self):
-    #     """no2"""
-    #     return self._no2
+    # 空气质量指数级别
+    @property
+    def category(self):
+        return self._category
 
-    # @property
-    # def co(self):
-    #     """co"""
-    #     return self._co
+    # pm10
+    @property
+    def pm10(self):
+        return self._pm10
+
+    # pm2.5    
+    @property
+    def pm25(self):
+        return self._pm25
+
+    # no2
+    @property
+    def no2(self):
+        return self._no2
     
-    # @property
-    # def so2(self):
-    #     """so2"""
-    #     return self._so2
-    
-    # @property
-    # def o3(self):
-    #     """o3"""
-    #     return self._o3
+    # so2
+    @property
+    def so2(self):
+        return self._so2
+
+    # co
+    @property
+    def co(self):
+        return self._co
+
+    # o3
+    @property
+    def o3(self):
+        return self._o3
     
     # 灾害预警
     @property
@@ -419,17 +421,17 @@ class WeatherData(object):
         self._vis        = weather["vis"]
         self._cloud      = weather["cloud"]
         self._dew        = weather["dew"]
+        self._qlty       = air["aqi"]
+        self._level      = air["level"]
+        self._primary    = air["primary"]
+        self._category   = air["category"]
+        self._pm10       = air["pm10"]
+        self._pm25       = air["pm2p5"]
+        self._no2        = air["no2"]
+        self._so2        = air["so2"]
+        self._co         = air["co"]
+        self._o3         = air["o3"]
         
-        # self._category = air["category"]
-        # self._pm25 = air["pm2p5"]
-        # self._pm10 = air["pm10"]
-        # self._primary = air["primary"]
-        # self._level = air["level"]
-        # self._no2 = air["no2"]
-        # self._so2 = air["so2"]
-        # self._co = air["co"]
-        # self._o3 = air["o3"]
-        # self._qlty = air["aqi"]
         allmsg=''
         titlemsg=''
         for i in disaster_warn:
